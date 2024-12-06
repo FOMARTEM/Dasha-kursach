e47e8b0feeeb3ad48f16874b1645df2125781b8d
Artem Shahov <shahov9ad@gmail.com>
2024-12-07 01:41:43 +0300

    main eeeee

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 /home/shahov/test_rar.rar
 /home/shahov/
-tJ&w
+abcd
+
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace chrono;
 typedef struct returns {
     string password;
     long long attempts;
+    int found;
 }returns;
 
 string getArchivePath() {
@@ -42,47 +43,44 @@ int extractArchive(const string& archivePath, const string& outputPath, const st
     
     //cout << result << endl;
     //2816 - неверный пароль, 2560 не верный путь, 0 всё хорошо
+    if (result == 2560) {
+        cout << "Неверное указан путь к файлу или место разархивирования" << endl;
+        exit(1);
+    } 
     return result;
 }
 
 returns FindPassword(const string& archivePath, const string& outputPath) {
-    //
-    string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    returns founded = {"", 0};
-    int found = 1;
+    //string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    string chars = "01234abcd";
+    returns founded = {"", 0, 1};
 
-    //какой то рандом
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distrib(0, chars.length() - 1);
-
-    //перебор
-    while (found != 0) {
-        string tpassword = "";
-        
-        for (int i = 0; i < 4; ++i) {
-            tpassword += chars[distrib(gen)];
+    //перебор      
+        for (char c1 : chars) {
+            for (char c2 : chars) {
+                for (char c3 : chars) {
+                    for (char c4 : chars) {
+                        founded.attempts++;
+                        string tpassword = "";
+                        tpassword += c1;
+                        tpassword += c2;
+                        tpassword += c3;
+                        tpassword += c4;
+                        cout << founded.attempts << "%: Пароль - " << tpassword << endl;
+                        founded.found = extractArchive(archivePath, outputPath, tpassword);
+                        if (founded.found == 0) {
+                            founded.password = tpassword;
+                            return founded;
+                        }
+                    }
+                }
+            }
         }
-
-        founded.attempts++;
-        
-        cout << founded.attempts << "%: Пароль - " << tpassword << endl;
-
-        found = extractArchive(archivePath, outputPath, tpassword);
-        if (found == 0) {
-            founded.password = tpassword;
-            return founded;
-        } else if (found == 2560) {
-            cout << "Неверное указан путь к файлу или место разархивирования" << endl;
-            exit(1);
-        } 
-
-    }
 
     return founded;
 
 }
-
 
 int main() {
     
@@ -93,55 +91,29 @@ int main() {
     string outputPath = getOutputPath();
 
     // данные
-    returns datas;
+    //returns datas;
+    returns datas = {"", 0, 1};
 
     cout << "Начало поиска пароля:" << endl;
-    
+
     auto start = high_resolution_clock::now();
 
     datas = FindPassword(archivePath, outputPath);
-
+    
+    //generatePasswords(archivePath, outputPath, "", datas);
+    
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
 
-    if (datas.password != "") {
+    if (datas.found == 0) {
         cout << "Найденный пароль: " << datas.password << endl;
         cout << "Количество попыток: " << datas.attempts << endl;
         cout << "Затрачено: " << duration.count() << " ms" << endl;
     } else {
         cout << "Ошибка: Пароль не найден (проверьте ввод)." << endl;
+        cout << "Затрачено: " << duration.count() << " ms" << endl;
+        return 1;
     }
     
     return 0;
 }
-
-
-
-
-
-/*
-bool extractArchive(const string& archivePath, const string& outputPath, const string& password) {
-    string command = "unrar x -p" + password + " \"" + archivePath + "\" \"" + outputPath + "\"";
-
-    // Выполняем команду
-    int result = system(command.c_str());
-
-    // Если результат выполнения команды не равен 0, значит, что-то пошло не так (неверный пароль и т.д.)
-    return (result == 0);
-}
-*/
-
-
-/*
-    while (success != 0) {
-        password = getPassword();
-        success = extractArchive(archivePath, outputPath, password);
-        
-        if (!success) {
-            cout << "Пароль неверный, попробуйте снова." << endl;
-        } else {
-            cout << "Архив успешно разархивирован!" << endl;
-        }
-    }
-*/
-
