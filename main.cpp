57163cc6ea04110d81fa0fe94e74dae613484772
Artem Shahov <shahov9ad@gmail.com>
2024-12-15 17:14:16 +0300

    progressbar adde in main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 using namespace chrono;
 
+int nowTry= 0;
+int tryes = 10000;
+
 typedef struct returns {
     string password;
     long long attempts;
@@ -26,13 +29,14 @@ string getOutputPath() {
     return path;
 }
 
+/*
 string getPassword() {
     string password;
     cout << "Введите пароль: ";
     getline(cin, password);
     return password;
 }
-
+*/
 
 int extractArchive(const string& archivePath, const string& outputPath, const string& password) {
     string command = "unrar x -p" + password + " \"" + archivePath + "\" \"" + outputPath + "\"";
@@ -49,36 +53,66 @@ int extractArchive(const string& archivePath, const string& outputPath, const st
     return result;
 }
 
+void printProgressBar (int attemp, const int combinations) {
+    int temp = combinations / tryes;
+    int ntf = nowTry * temp;
+    int percent = (attemp - ntf)  * 100 /  temp;
+    int pos = 50; //один символ 2%
+    int filled = percent * pos / 100; 
+    //printf("attemp: %d, combinations: %d temp: %d, ntf: %d, percent: %d, filled: %d\n", attemp, combinations, temp, ntf, percent, filled);
+    cout << "\rПрогресс: ["; // Выводим начало прогресс-бара \r
+    //вывод заполнености текущей стадии
+    for (int i = 0; i < pos; i++) {
+        if (i < filled) {
+            cout << '%';
+        } else {
+            cout << '.'; 
+        }
+    }
+    //конец вывода
+    cout << "] ";// << nowTry + 1 << " of " << tryes; // Выводим начало прогресс-бара
+    cout.flush(); // Сбрасываем буфер, чтобы увидеть изменения немедленно
+    nowTry = attemp/temp;
+}
+
 returns FindPassword(const string& archivePath, const string& outputPath) {
-    //string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-    string chars = "abcd";
+    //const char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; //63 - 15752961
+    const string chars = "abcdefghijklmnopqrstuvwxyz0123456789"; //37 - 1874161
+    //const string chars = "abcd"; //4 - 256
     returns founded = {"", 0, 1};
 
+    const int len = chars.length(); //длина символов
+    const int combinations = len*len*len*len; //кол-во комбинаций
+
     //перебор      
-        for (char c1 : chars) {
-            for (char c2 : chars) {
-                for (char c3 : chars) {
-                    for (char c4 : chars) {
-                        founded.attempts++;
-                        string tpassword = "";
-                        tpassword += c1;
-                        tpassword += c2;
-                        tpassword += c3;
-                        tpassword += c4;
-                        cout << founded.attempts << "%: Пароль - " << tpassword << endl;
-                        founded.found = extractArchive(archivePath, outputPath, tpassword);
-                        if (founded.found == 0) {
-                            founded.password = tpassword;
-                            return founded;
-                        }
+    for (char c1 : chars) {
+        for (char c2 : chars) {
+            for (char c3 : chars) {
+                for (char c4 : chars) {
+                    founded.attempts++;
+                    string tpassword = "";
+                    tpassword += c1;
+                    tpassword += c2;
+                    tpassword += c3;
+                    tpassword += c4;
+
+                    //cout << founded.attempts << "%: Пароль - " << tpassword << endl; //вывод пароля
+                    printProgressBar(founded.attempts, combinations);
+                    
+                    founded.found = extractArchive(archivePath, outputPath, tpassword);
+                    if (founded.found == 0) {
+                        founded.password = tpassword;
+                        cout << "\r\033[KПрогресс: 100% пароль найден" << endl;
+                        //cout << endl << "Прогресс: 100% пароль найден" << endl; //если не нужно чистить прогрессбар
+                        
+                        return founded;
                     }
                 }
             }
         }
+    }
 
     return founded;
-
 }
 
 int main() {
@@ -98,8 +132,6 @@ int main() {
     auto start = high_resolution_clock::now();
 
     datas = FindPassword(archivePath, outputPath);
-    
-    //generatePasswords(archivePath, outputPath, "", datas);
     
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
